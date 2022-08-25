20965df2599de2d7686aa9a772d997ad64becb73
Harry AzraDevOps <72076704+AzraDevOps@users.noreply.github.com>
2022-08-25 08:05:38 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+game 'gta5'
+fx_version 'cerulean'
+
+author: Harry Meyers (Harry MEYERS#6408) forked Virakal 
+
+description: Add blips filter by jobs
+
+version: 1.0.0
+
+--git of this resource : https://github.com/AzraDevOps/FiveM-Player-JobBlips
+--forked from : https://github.com/Virakal/FiveM-Player-Blips
+
+
+client_script {
+	'config/config.lua'
+	'client/main_cl.lua'
+}
+
+server_script {
+--	nothing
+}
